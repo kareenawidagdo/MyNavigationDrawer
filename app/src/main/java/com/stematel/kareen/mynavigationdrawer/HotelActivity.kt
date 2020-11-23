240ae90d370392e6fc32b65933f3f25cb7ddb76b
kareenawidagdo <widagdo.kareena@gmail.com>
2020-11-23 11:17:19 +0700

    first commit

---
+++ app/src/main/java/com/stematel/kareen/mynavigationdrawer/HotelActivity.kt
@@ -0,0 +1,11 @@
+package com.stematel.kareen.mynavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel)
+    }
+}
